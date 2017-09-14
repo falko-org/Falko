59a762dfbd1ec984d041a0b8e699152f1952b984
MatheusRich <matheusrichardt@gmail.com>
2017-09-14 16:16:02 -0300

    Modeling login component
    Signed-off-by: Mateus097 <mat97_@hotmail.com>

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,27 @@
 <template>
-
+  <div class="card">
+    <div class="card-header">
+      <ul class="nav nav-fill">
+        <li class="nav-item">
+          <a class="nav-link">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Disabled</a>
+        </li>
+      </ul>
+    </div>
+    <div class="card-body">
+      <form id="login">
+        <div class="form-group">
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Log In</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,4 +36,9 @@ export default {
 </script>
 
 <style>
+#login input {
+  border: none;
+  border-radius: 0;
+  border-bottom: solid #c0c0c0 thin;
+}
 </style>
