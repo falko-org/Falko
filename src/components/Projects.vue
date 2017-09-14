b8a27293178c1e82710924cc530ebef1590cc4f7
Adrianne <driah_as@hotmail.com>
2017-09-14 16:02:01 -0300

    Creating projects listing component
    Signed-off-by: Adrianne Alves <driah_as@hotmail.com>
    Signed-off-by: Leonardo Dos Santos <leossb36@gmail.com>
    Signed-off-by: Matheus Bernardo <matheussbernardo@gmail.com>

---
+++ src/components/Projects.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div v-for="project in projects" class="card">
+      <div class="card-body">
+        <div class="card-title">{{project.name}}</div>
+        <div class="card-text">{{project.description}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'projects',
+  data(){
+    return{
+      projects: [{name: "projeto1", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac lorem eros. Praesent gravida augue sit amet sem semper faucibus. Nulla convallis nunc odio, eu dapibus dui semper vel. Donec mi mi, rutrum sed venenatis id, auctor vitae nisi. Praesent bibendum eros vel orci lacinia ullamcorper."},{name: "projeto2", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac lorem eros. Praesent gravida augue sit amet sem semper faucibus. Nulla convallis nunc odio, eu dapibus dui semper vel. Donec mi mi, rutrum sed venenatis id, auctor vitae nisi. Praesent bibendum eros vel orci lacinia ullamcorper."},{name: "projeto3", description: "dipiscing elit. Maecenas ac lorem eros. Praesent gravida augue sit amet sem semper faucibus. Nulla convallis nunc odio, eu dapibus dui semper vel. Donec mi mi, rutrum sed venenatis id, auctor vitae nisi. Praesent bibendum eros vel orci lacinia ullamcorper."}]
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Projects from '@/components/Projects'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Projects',
+      component: Projects
     }
   ]
 })
