b579f5fbbcc4ca2d390bed606cad28979795cc19
MatheusRich <matheusrichardt@gmail.com>
2017-09-15 17:44:40 -0300

    Restyling login component
    Signed-off-by: MateusO97 <mat97_@hotmail.com>

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,26 +1,18 @@
 <template>
-  <div class="card">
-    <div class="card-header">
-      <ul class="nav nav-fill">
-        <li class="nav-item">
-          <a class="nav-link">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link">Disabled</a>
-        </li>
-      </ul>
-    </div>
-    <div class="card-body">
-      <img src="../assets/twitter.png" class="rounded mx-auto d-block" id="falkoLogoLogin">
+  <div class="card" id="login">
+    <div class="card-body tab-pane fade show active" id="loginPanel" role="tabpanel" aria-labelledby="pills-home-tab">
+      <img src="../assets/logo.png" class="rounded mx-auto d-block" id="falkoLogoLogin">
 
-      <form id="login">
+      <form id="loginForm">
         <div class="form-group">
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
         </div>
         <div class="form-group">
           <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
-        <button type="submit" class="btn btn-primary">Log In</button>
+        <div class="text-center">
+          <button type="submit" class="btn btn-primary" id="loginButton">Log In</button>
+        </div>
       </form>
     </div>
   </div>
@@ -38,7 +30,11 @@ export default {
 </script>
 
 <style>
-#login input {
+#login {
+  max-width: 20em;
+}
+
+#loginForm input {
   border: none;
   border-radius: 0;
   border-bottom: solid #c0c0c0 thin;
@@ -46,5 +42,16 @@ export default {
 
 #falkoLogoLogin {
   width: 11em;
+  margin: 2.5em 0;
+}
+#loginButton {
+  align-self: center;
+  margin: 0.5em 0;
+  border-radius: 2em;
+  padding: 0.5em 2em;
+  cursor: pointer;
+  background-color: #86B1B1;
+  border-width: 0;
+  font-weight: bold;
 }
 </style>
