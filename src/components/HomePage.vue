af8c1019cb9278b342a8374ff7ffbeb9347bebe0
MatheusRich <matheusrichardt@gmail.com>
2017-09-21 16:38:13 -0300

    Adding parallax

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,25 +1,65 @@
 <template>
   <div>
     <div  id="sectionBlue">
-      <div class="row justify-content-around">
-        <div class="col-sm-6 align-self-center" id="loginComponent">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div class="row justify-content-around center-block">
+
+        <div class="col-sm-5 align-self-center" id="loginComponent">
+          <h1>See beyond</h1>
+          <p class="white-description">
+            The Falko platform was designed with the focus on meeting the need for agile
+            project managers to track the productivity of teams and projects managed in a
+            more simplified way. The application aims to present the metrics established,
+            through the data collected in GitHub, as relevant information and compact
+            reports. Thus, it will centralize all the information necessary for the analysis
+            and decision making of the manager.
+          </p>
         </div>
 
-        <div class="col-sm-4" id="loginComponent">
+        <div class="col-sm-4" id="loginComponent" align="center">
+          <h2>Start Now!</h2>
           <login></login>
         </div>
       </div>
     </div>
 
+      <div class="parallax">
+        <div class="row justify-content-around center-block" id="asdf">
+          <div class="col-sm-5 align-self-center" id="loginComponent">
+            <h1>See beyond</h1>
+            <p class="white-description">
+              The Falko platform was designed with the focus on meeting the need for agile
+              project managers to track the productivity of teams and projects managed in a
+              more simplified way. The application aims to present the metrics established,
+              through the data collected in GitHub, as relevant information and compact
+              reports. Thus, it will centralize all the information necessary for the analysis
+              and decision making of the manager.
+            </p>
+          </div>
+
+          <div class="col-sm-4" id="loginComponent" align="center">
+            <h1>Start Now!</h1>
+            <login></login>
+          </div>
+        </div>
+      </div>
+
     <div class="" id="sectionLight">
-      <div class="row justify-content-around">
-        <div class="col-sm-4" id="registerComponent">
+      <div class="row justify-content-center">
+        <div class="col-sm-4" align="center" id="registerComponent">
           <register></register>
         </div>
 
+
         <div class="col-sm-6 align-self-center">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <h1>Another Lorem Ipsum</h1>
+          <p class="white-description">
+            The Falko platform was designed with the focus on meeting the need for agile
+            project managers to track the productivity of teams and projects managed in a
+            more simplified way. The application aims to present the metrics established,
+            through the data collected in GitHub, as relevant information and compact
+            reports. Thus, it will centralize all the information necessary for the analysis
+            and decision making of the manager.
+          </p>
         </div>
 
       </div>
@@ -46,6 +86,42 @@ export default {
 </script>
 
 <style scoped>
+
+h1 {
+  color: white;
+  text-shadow: 1px 2px rgba(0, 0, 0, 0.4);
+}
+
+.parallax {
+    /* The image used */
+    background-image: url("../assets/fk.jpg");
+
+    /* Set a specific height */
+    height: auto;
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+}
+
+.white-description {
+  color: #EFF3F5;
+  text-align: justify;
+}
+
+#asdf {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(18, 69, 89, 0.7);
+}
+
+#falkoDescription {
+  color: #EFF3F5;
+}
+
 #loginComponent {
   padding-top: 2em;
   padding-bottom: 2em;
