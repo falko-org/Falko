49ce95fc5b8bf87db948b55355ab835e94dc65ee
Adrianne <driah_as@hotmail.com>
2017-09-21 21:53:10 -0300

    Improving navbar-top
    Signed-off-by: Adrianne <driah_as@hotmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,30 @@
 <template>
   <div id="app">
+
+     <nav class="navbar topo static-top fixed-top navbar-expand-lg container.fluid">
+            <div class = "navbar-header">
+              <a class="navbar-brand" href="#">
+                <img src = "./assets/Falko - Completo.png" width="70" height="50"/>
+              </a>
+            </div>
+            <div class="form pesquisa my-2 my-lg-0">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+      </nav>    
     <router-view>
     </router-view>
-    <nav class="navbar static-top fixed-top navbar-expand-lg navbar-fixed-top">
-      <div class = "navbar-header">
-        <a class="navbar-brand" href="#"><img src="assets/Falko-Final.png" width="40" height="40"/></a>
-      </div>
-      <div class="collapse navbar-collapse">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <ul class= "nav navbar-nav navbar-right">
-          <li><a href="#" toggle="modal" id = "addp"><button type="button" class="btn btn-default navbar-btn bnt-primary">Adicionar </button></a></li>
-          <li><a href="#" toggle="modal" id = "editp"><button type="button" class="btn btn-default navbar-btn">Editar</button></a></li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="modal fade" role="dialog" id="add">
-      <div class="modal-dialog" >
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Adicionar/Editar Projeto</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body ">
-
-            <div class = "column-3 column label">
-              <label class="name"> Nome </label>
-            </div>
-            <div class = "column-9 column input">
-              <input type = "text" id = "nameproject"></input><br>
-            </div>
-            <div class = "column-3 column label">
-              <label> Descrição </label>
-            </div>
-            <div class = "column-9 column input">
-              <input type = "text" id= "descriptionproject"></input>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Salvar</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'app'
 }
-$(document).ready(function(){
-    $("#addp").click(function(){
-        $("#add").modal();
-    });
-});
-
-$(document).ready(function(){
-    $("#editp").click(function(){
-        $("#add").modal();
-    });
-});
 
 </script>
 
-<style scoped>
+<style>
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -78,73 +32,25 @@ $(document).ready(function(){
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 110px;
+  margin-top: 100px;
 }
-.navbar{
+
+.topo{
   padding: 0.2%;
   width: 100%;
-  background-color: #46555B;
-}
-
-.nav{
-    position: absolute;
-    width: 100%;
-    left: 85%;
-    text-align: center;
-    margin:0 auto;
-}
-
-.form-group{
-  position: absolute;
-  left: 42%;
-  top : 20%;
-}
-
-.btn{
-  margin-right: 10px;
+  background-color: #E8E8E8;
 }
 
 .navbar-brand{
-  padding-left: 50px;
+  padding-left: 2%;
   height: 100%;
 }
-.modal-body{
-  position: relative;
-  top: 5px;
 
-}
-.modal{
-  position: absolute;
-  top: 100px;
-}
-.column-3 {
-    width: 25%;
-}
-
-.column-9 {
-    width: 75%;
-}
-
-.label {
-    padding-top: 7px;
-    text-align: right;
-}
-
-.label:after {
-    content: ':';
-    padding-right: 5px;
-}
-
-.column {
-    float: left;
-    height: 41px;
-}
-
-.input input {
-    width: 100%;
-    height: 34px;
-    padding-left: 5px;
-    box-sizing: border-box;
+.pesquisa{
+  position: fixed;
+  left: 30%;
+  right: 30%;
+  top : 4px;
 }
 
 </style>
