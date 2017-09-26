a07df1829770bd9b4ddf00519de0ade241c22728
Leossb <leossb36@gmail.com>
2017-09-26 15:44:51 -0300

    Creating InProject component

---
+++ src/components/InProject.vue
@@ -0,0 +1,32 @@
+<template>	
+	<div class="container">
+		<div class="row inproject">
+			<div class="card">
+				<div class="card-body">
+					<div class="card-title">{{project.name}}</div>
+						<div class="card-text">{{project.description}}</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</template>
+
+<script>
+export default{
+	name:'InProject',
+	data(){
+		return{
+			project: []
+		}
+	}
+}
+</script>
+	
+<style scoped>
+.inproject{
+	margin: 30em 0 0 0;
+	text-align: center;
+}
+
+</style>
