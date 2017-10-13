e92d9f8e20ef8482ae5436f00ae1f5a4a93922ea
Matheus Bernardo <matheussbernardo@gmail.com>
2017-10-12 22:47:06 -0300

    Starting github api authentication
    Signed-off-by: Matheus Bernardo <matheussbernardo@gmail.com>
    Signed-off-by: Daniel Oda  <danieloda35@gmail.com>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,11 +4,32 @@
         <img src="logo.png" width="50"/>
         <img src="../assets/falko-text.png" height="50"/>
       </a>
+
+      <button v-on:click="link()">CLICK PARA LINKAR</button>
+
   </nav>
 </template>
 
 <script>
-export default {  
+import axios from 'axios';
+import {HTTP} from '../http-common.js';
+
+export default {
+	methods: {
+		link() {
+			window.location.replace("https://github.com/login/oauth/authorize?scope=repo&client_id=cbd5f91719282354f09b");
+		}
+	},
+	mounted() {
+		var code = window.location.search.split("=")[1];
+		HTTP.post('',{
+			code: this.code
+		})
+		.then((response) => {
+		})
+		.catch()
+
+	}	
 }
 </script>
 
