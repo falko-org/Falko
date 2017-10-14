d49731386d038f3130623d5eee75c569eb97b32c
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-14 18:04:32 -0300

    Creating github call back component
    Signed-off-by: Vinícius Cantuária <cantuariavc@gmail.com>
    Signed-off-by: Daniel Oda <danieloda35@gmail.com>
    Signed-off-by: Matheus Bernardo <matheussbernardo@gmail.com>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,25 +11,12 @@
 </template>
 
 <script>
-import axios from 'axios';
-import {HTTP} from '../http-common.js';
-
 export default {
-	methods: {
-		link() {
-			window.location.replace("https://github.com/login/oauth/authorize?scope=repo&client_id=cbd5f91719282354f09b");
-		}
-	},
-	mounted() {
-		var code = window.location.search.split("=")[1];
-		HTTP.post('',{
-			code: this.code
-		})
-		.then((response) => {
-		})
-		.catch()
-
-	}	
+  methods: {
+    link() {
+      window.location.replace("https://github.com/login/oauth/authorize?scope=repo&client_id=cbd5f91719282354f09b");
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Register from '@/components/Register'
 import LoginRegister from '@/components/LoginRegister'
 import HomePage from '@/components/HomePage'
 import NoProjects from '@/components/NoProjects'
+import GitHubCallBack from '@/components/GitHubCallBack'
 
 Vue.use(Router)
 
@@ -56,6 +57,12 @@ export default new Router({
       path: '/loginregister',
       name: 'LoginRegister',
       component: LoginRegister
+    },
+
+    {
+      path: '/githubcallback',
+      name: 'GitHubCallBack',
+      component: GitHubCallBack
     },
 
     {
