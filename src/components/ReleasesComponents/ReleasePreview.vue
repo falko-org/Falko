72c8ce568d9eaa6d2e911e33b5ee4bd0f03ca5da
Matheus456 <matheus456456@gmail.com>
2017-10-17 21:36:14 -0200

    Styling ReleasePreview component
    Signed-off-by: MatheusRich <matheusrichardt@gmail.com>

--- src/components/ReleasesComponents/ReleasePreview.vue
+++ src/components/ReleasesComponents/ReleasePreview.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="card" id="releaseCard">
-    <div class="card-header" id="releaseHeader">  
+    <div class="card-header" id="releaseHeader">
       <div class="row align-itens-around" id="releaseTitle">
         <div class="col">
           <h4 class="no-margin">{{releaseName}}</h4>
         </div>
         <div class="col">
+          <img src="../../assets/dateicon.png" width="25em" alt="Date icon"/>
           <h5 class="no-margin float-right">
-          {{releaseInitalDate}}
-          {{releaseFinalDate}}
+            {{releaseInitalDate}}
+            {{releaseFinalDate}}
           </h5>
         </div>
       </div>
     </div>
     <div class="card-body">
-      <p class="card-text">
-        {{releaseDescription}}
-      </p>
+      <div class="row align-itens-arround">
+        <div class="col-5">
+          <p class="card-text">
+            <div class="number-circle"><div id="amountSprintsFont">{{releaseAmountOfSprints}}</div></div>
+            <h5 style="text-align:center"><br>Sprints</h5>
+          </p>
+        </div>
+        <div class="col">
+          <p class="card-text text-justify">
+            {{releaseDescription}}
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,7 +40,7 @@ export default {
       releaseDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et consequatur porro repellendus vero mollitia corrupti facilis in",
       releaseInitalDate: "10/10/10",
       releaseFinalDate: "13/10/12",
-      releaseAmountOfSprints: "7"
+      releaseAmountOfSprints: "8"
     }
   }
 }
@@ -41,15 +52,48 @@ export default {
 }
 
 #releaseHeader {
-  background-color: white;
+  background-color: #7799A5;
 }
 
 #releaseTitle {
   margin: 0;
+  color: white;
+}
+
+#numberSprints {
+  margin-left: -10;
+}
+
+#amountSprintsFont {
+  font-weight:bolder;
+  font-size:1.4em;
 }
 
 .no-margin {
   margin: 0;
 }
+
+.number-circle {
+  font: 32px Arial, sans-serif;
+
+  width: 2.5em;
+  height: 2.5em;
+  box-sizing: initial;
+
+  background: #fff;
+  border: 0.2em solid #7799A5;
+  border-style: solid;
+  color: #7799A5;
+  text-align: center;
+  border-radius: 50%;
+
+  line-height: 2.5em;
+  box-sizing: content-box;
+  top: 50%;
+  left: 50%;
+  margin-left: 40px;
+  margin-top: -20px;
+}
+
 
 </style>
