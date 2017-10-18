84ed9a1561522bb12426ddf9276d9c9f0c48d8cd
Álax Alves <alaxallves@gmail.com>
2017-10-17 22:45:53 -0200

    Adding getReleases method
    Signed-off-by: Matheus456 <matheus456456@gmail.com>
    Signed-off-by: MatheusRich <matheusrichardt@gmail.com>

--- src/components/ReleasesComponents/Releases.vue
+++ src/components/ReleasesComponents/Releases.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="row justify-content-around">
-    <div class="col-5">
-      <div align="center">
-        <release-preview></release-preview>
+    <div class="row top-buffer" v-for="i in Math.ceil(releases.length / 2)">
+      <div v-for="release in releases.slice((i-1) * 2,i*2)" class="col-5">
+        <div align="center">
+          <release-preview></release-preview>
+        </div>
       </div>
     </div>
   </div>
@@ -18,9 +20,22 @@ export default {
 
   data () {
     return {
-
+      releases: {}
     }
-  }  
+  },
+
+  methods: {
+    getReleases() {
+      HTTP.get(`projects/${this.$route.params.id}/releases`)
+        .then((response) => {
+          this.releases = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    }
+  }
+
 }
 </script>
 
