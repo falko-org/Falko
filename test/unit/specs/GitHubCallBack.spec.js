20fa6c4d61bff85bf4ba4d3522725a50b9757752
Matheus Bernardo <matheussbernardo@gmail.com>
2017-10-20 14:41:58 -0200

    Adding front test with mocha, chai and sinon

---
+++ test/unit/specs/GitHubCallBack.spec.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import GitHubCallBack from '../../../src/components/GitHubCallBack.vue'
+import {HTTP} from '../../../src/http-common.js';
+import sinon from 'sinon';
+
+describe('On github callback', function() {
+
+  it('should have the correct token', function (done) {
+  	const response = new Promise((r) => r(
+  			{
+  				data: {access_token: "1234"}
+  			}));
+
+  	sinon.stub(HTTP, 'post').returns(response);
+
+  	const Ctor = Vue.extend(GitHubCallBack)
+    const vm = new Ctor().$mount()
+
+    process.nextTick(function() {
+   		expect(vm.token).to.equal('1234');
+   		done();
+    });
+   
+  })
+})
--- test/unit/specs/Hello.spec.js
+++
@@ -1,5 +0,0 @@
-describe('HomePage.vue', () => {
-  it('should render correct contents', () => {
-    expect(true).toBe(true)
-  })
-})
