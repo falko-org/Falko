3ca4679347fb752ebecb8b97e5910405f27da148
Vinícius Cantuária <cantuariavc@gmail.com>
2017-10-28 09:01:29 -0200

    Adding user id and request GitHub token authenticate in frontend

--- src/components/GitHubCallBack.vue
+++ src/components/GitHubCallBack.vue
@@ -1,33 +1,38 @@
 <template>
-<div>
-<h1>{{token}}</h1>
-</div>
+  <div>
+    <h1> {{token}} </h1>
+  </div>
 </template>
 
 <script>
-import {HTTP} from '../http-common.js';
+import { HTTP } from '../http-common.js';
 
 export default {
   data() {
     return {
-      token: ""
-    }
+      token: '',
+    };
   },
   mounted() {
-    var code = window.location.search.split("=")[1];
-    HTTP.post('request_github_token',{
+    var raw_token = localStorage.getItem('token');
+    var token = raw_token.replace(/"/, "").replace(/"/, "");
+    var headers = { 'Authorization': token };
+    var user_id = localStorage.getItem('user_id');
+
+    var code = window.location.search.split('=')[1];
+    HTTP.post('request_github_token', {
       code: code,
-      id: "1"
-    })
-    .then((response) => {
-      console.log(response.data.access_token);
-      this.token = response.data.access_token
-    })
-    .catch((e) => {
-      this.errors.push(e);
-    });
-  }
-}
+      id: user_id,
+    }, { headers: headers })
+      .then((response) => {
+        console.log(response.data.access_token);
+        this.token = response.data.access_token;
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
+};
 </script>
 
 <style>
