f89e77ba5ba898e04c41eb6875fed41d24eb1d86
Vinícius Cantuária <cantuariavc@gmail.com>
2017-11-04 02:57:52 -0200

    Showing User informations in edit

--- src/components/Users/EditUserProfile.vue
+++ src/components/Users/EditUserProfile.vue
@@ -1,78 +1,99 @@
 <template>
-	<div class="edit-user-profile">
-		<button type="button" class="btn btn-info btn-md falko-button" id="editbutton" data-toggle="modal" data-target="#editModal">
-			Edit
-		</button>
+  <div class="edit-user-profile">
+    <button type="button" class="btn btn-info btn-md falko-button" id="editbutton" data-toggle="modal" data-target="#editModal">
+      Edit
+    </button>
 
-		<div class="modal fade" id ="editModal" role="dialog">
-			<div class="modal-dialog">
-		    	<div class="modal-content">
-			        <div class="modal-header">
-		          	<h4 class="modal-title">Edit User Profile</h4>
-			          	<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-			            	<span aria-hidden="true">&times;</span>
-			            </button>
-			        </div>
-			        <div class="modal-body">
-						<p><label > Name </label></p>
-						<p><input type = "text" v-model="name"></input><br></p>
-						<p><label> E-mail </label></p>
-						<p><input type = "text" v-model="email"></input><br></p>
-						<p><label> GitHub Account </label></p>
-						<p><input type = "text" v-model="github"></input><br></p>
-			        </div>
-			        <div class="modal-footer">
-			          	<button type="button" class="btn btn-primary" v-on:click="editUser" data-dismiss="modal">Save</button>
-		        	  	<button type="button" class="btn btn-secondary" data-dismiss="modal" >Close</button>
-			        </div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="modal fade" id ="editModal" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Edit User Profile</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                <span aria-hidden="true">&times;</span>
+              </button>
+          </div>
+          <div class="modal-body">
+            <p><label > Name </label></p>
+            <p><input type = "text" v-model="name"></input><br></p>
+            <p><label> E-mail </label></p>
+            <p><input type = "text" v-model="email"></input><br></p>
+            <p><label> GitHub Account </label></p>
+            <p><input type = "text" v-model="github"></input><br></p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" v-on:click="editUser" data-dismiss="modal">Save</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" >Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 	import { HTTP } from '../../http-common';
 	import { EventBus } from '../../event-bus.js';
 
-	export default {
-		name: "edit-user-profile",
-		data() {
-			return {
-				name: "",
-				email: "",
-				github: ""
-				//is_github_authenticated: false
-			}
-		},
-		methods: {
-			editUser() {
-				var raw_token = localStorage.getItem('token');
-				var token = raw_token.replace(/"/, "").replace(/"/, "");
+  export default {
+    name: 'edit-user-profile',
+    data() {
+      return {
+        name: '',
+        email: '',
+        github: '',
+        // is_github_authenticated: false,
+      };
+    },
+    methods: {
+      editUser() {
+        var token = localStorage.getItem('token');
+        var tokenSimple = token.replace(/"/, "");
+        var tokenSimple2 = tokenSimple.replace(/"/, "");
+        var headers = { 'Authorization': tokenSimple2 };
+        var userId = localStorage.getItem('user_id');
 
-				var headers = { 'Authorization':token };
+        console.log(userId);
+        console.log(token);
 
-	      var user_id = localStorage.getItem('user_id');
+        HTTP.put(`users/${userId}`, {
+          user: {
+            name: this.name,
+            email: this.email,
+            github: this.github,
+          },
+        }, { headers: headers })
+          .then(() => {
+            EventBus.$emit('edited-user-profile', 1);
+          })
+          .catch((e) => {
+            this.errors.push(e);
+          });
+      },
 
-	      console.log(user_id);
-	      console.log(token);
+      getUserInformation() {
+        var token = localStorage.getItem('token');
+        var tokenSimple = token.replace(/"/, "");
+        var tokenSimple2 = tokenSimple.replace(/"/, "");
+        var headers = { 'Authorization': tokenSimple2 };
+        var userId = localStorage.getItem('user_id');
 
-				HTTP.put("users/"+user_id, {
-					user: {
-							name: this.name,
-							email: this.email,
-							github: this.github
-					}
-				}, { headers: headers })
-				.then((response) => {
-					EventBus.$emit('edited-user-profile', 1)
-				})
-				.catch((e) => {
-					this.errors.push(e);
-				})
-			}
-		}
-	}
+
+        HTTP.get(`users/${userId}`, { headers: headers })
+          .then((response) => {
+            this.name = response.data.name;
+            this.email = response.data.email;
+            this.github = response.data.github;
+          })
+          .catch((e) => {
+            this.errors.push(e);
+          });
+      },
+    },
+    created() {
+      this.getUserInformation();
+    },
+  };
 </script>
 
 <style scoped>
