a56123a11f37a711081c484bec0a3b0fa41f2ed5
MateusO97 <mat97_@hotmail.com>
2017-11-05 18:25:42 -0200

    Adding Sidebar routes to App
    Signed-off-by: leossb36 <leossb36@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,7 @@ import UserProfile from '@/components/Users/UserProfile';
 import EditUserProfile from '@/components/Users/EditUserProfile';
 import DeleteUserProfile from '@/components/Users/DeleteUserProfile';
 import NotFound from '@/components/NotFound';
+import Sidebar from '@/components/Sidebar';
 
 Vue.use(Router);
 
@@ -143,6 +144,11 @@ const router = new Router({
       path: '/notFound',
       name: 'NotFound',
       component: NotFound,
+    },
+    {
+      path: '/sidebar',
+      name: 'Sidebar',
+      component: Sidebar,
     },
   ],
 });
