35cd6cdab3ac823d9ad6df004685d538d8c2996d
MatheusRich <matheusrichardt@gmail.com>
2017-11-11 13:37:58 -0200

    Fixing media queries

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,10 +1,10 @@
-<template lang="html">
-  <div class= "row justify-content-around" id="notfound">
-    <div class="col-md-8" align="center">
-        <img src="../assets/bw-logo.png" class="img-fluid" id="logo"/>
-        <h1 class="no-margin">404</h1>
-        <h2 class="no-margin">Oops!</h2>
-        <h3 class="text-muted no-margin"> Requested page not found!</h3><br>
+<template>
+  <div class= "row justify-content-around no-margin" id="notfound">
+    <div class="col-md-8 align-self-center" align="center">
+      <img src="../assets/bw-logo.png" class="img-fluid align-self-center"/>
+      <h1 class="no-margin align-self-center">404</h1>
+      <h2 class="no-margin align-self-center">Oops!</h2>
+      <h3 class="text-muted no-margin align-self-center"> Requested page not found!</h3>
     </div>
   </div>
 </template>
@@ -18,44 +18,48 @@ export default {
 h1 {
     color: #182B32;
     font-weight: bold;
-    padding-top: 0.6em;
-    padding-bottom: 0.5em;
+    padding-top: 0.5em;
     font-size: 3.5em;
 }
 h2 {
     color: #0E3341;
     font-style: italic;
-    padding-bottom: 0.2em;
 }
 h3 {
     font-size: 1.2em;
 }
 
 #notfound{
-    margin: 9em 0;
+  height: 32.4em;
 }
 
 @media (max-width: 480px) {
      .img-fluid {
-       width: 10em;
+       width: 5em;
      }
  }
 
 @media (max-width: 767px) {
   .img-fluid {
-    width: 15em;
+    width: 10em;
   }
 }
 
-@media (min-width: 768px) and (max-width: 979px) {
+@media (min-width: 768px) {
   .img-fluid {
-    width: 24em;
+    width: 12.5em;
   }
 }
 
 @media (min-width: 1200px) {
   .img-fluid {
-    width: 23em;
+    width: 15em;
+  }
+}
+
+@media (min-width: 1500px) {
+  .img-fluid {
+    width: 20em;
   }
 }
 
