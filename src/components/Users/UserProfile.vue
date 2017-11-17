0b9577fb1be44780d4b0513b2848a985f3a3adfc
Leonardo Dos Santos Silva Barreiros <leossb36@gmail.com>
2017-11-17 15:33:09 -0200

    Improving UserProfile
    Signed-off-by: Leonardo Dos Santos Silva Barreiros <leossb36@gmail.com>

--- src/components/Users/UserProfile.vue
+++ src/components/Users/UserProfile.vue
@@ -1,46 +1,52 @@
-<template>
-  <div class="col">
-    <div class="row">
-      <div class="card mx-auto">
-        <div class="card-body project">
-          <div class="col">
-            <div class="row">
-              <h4 class="card-title"> User Profile </h4>
+<template>  
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="card mx-auto"> 
+        <div class="card-body project" id="userProfile">
+          <h4 class="row justify-content-center card-title"> User Profile </h4>
+          <div class="image-circle row justify-content-center">
+            <img src="../../assets/user.png" height="60" />
+          </div>
+          <div class="row justify-content-center">
+            <div class="card-body">
+              <div class="row">
+                <label> Name: </label>
+                <p class="card-text text-muted">{{name}}</p>
+              </div>
+              <div class="row">
+                <label> E-mail: </label>
+                <p class="card-text text-muted">{{email}}</p>
+              </div>
+              <div class="row">
+                <label> GitHub Account: </label>
+                <p class="card-text text-muted">{{github}}</p>
+              </div>
+              <div class="row">
+                <label> GitHub Authenticated: </label>
+                <p class="card-text text-muted">{{is_github_authenticated}}</p>
+              </div>
             </div>
-            <div class="row">
-              <label> Name: </label>
-              <p class="card-text text-muted">{{name}}</p>
-            </div>
-            <div class="row">
-              <label> E-mail: </label>
-              <p class="card-text text-muted">{{email}}</p>
-            </div>
-            <div class="row">
-              <label> GitHub Account: </label>
-              <p class="card-text text-muted">{{github}}</p>
-            </div>
-            <div class="row">
-              <label> GitHub Authenticated: </label>
-              <p class="card-text text-muted">{{is_github_authenticated}}</p>
+          </div>    
+          <div class="card-text">
+            <div class="row justify-content-center">
+              <div class="col-md-7">
+                <button v-bind:class="buttonClass()" v-on:click="link()">
+                  Link to Github
+                </button>
+              </div>            
+              <div class="col">
+                <edit-user-profile></edit-user-profile>
+              </div>
+              <div class="col">
+                <delete-user-profile></delete-user-profile>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="row justify-content-center">
-      <div class="col-md-2">
-        <edit-user-profile></edit-user-profile>
-      </div>
-      <div class="col-md-2">
-        <delete-user-profile></delete-user-profile>
-      </div>
-      <div>
-        <button v-bind:class="buttonClass()" v-on:click="link()">
-          Link to Github
-        </button>
-      </div>
-    </div>
   </div>
+  
 </template>
 
 <script>
@@ -86,17 +92,14 @@ export default {
           this.errors.push(e);
         });
     },
-
     link() {
       if (!this.isGitHubLinked()) {
         window.location.replace('https://github.com/login/oauth/authorize?scope=repo&client_id=cbd5f91719282354f09b');
       }
     },
-
     isGitHubLinked() {
       return this.is_github_authenticated;
     },
-
     buttonClass() {
       if (this.isGitHubLinked()) {
         return 'btn btn-info btn-md falko-button-grey disabled-cursor';
@@ -116,11 +119,10 @@ export default {
 </script>
 
 <style scoped>
-label {
-  margin-right: 5px;
-}
-
-#buttons {
-  margin-top: 1em;
-}
+  .card {
+    box-shadow: 0 0.2em 0.7em -0.1em #999999;
+  }
+  label {
+    margin-right: .3em;
+  }   
 </style>
