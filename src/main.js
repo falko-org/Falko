405e2ad78a89436dbb26682ba8eac975748b04d4
Matheus Roberto <matheus456456@gmail.com>
2017-11-21 20:37:07 -0200

    Adding package of VueChart
    Signed-off-by: Matheus Roberto <matheus456456@gmail.com>
    Signed-off-by: pedrokelvin <pedrokelvin2006@gmail.com>
    Signed-off-by: drianne <driah_as@hotmail.com>
    Signed-off-by: MartinsLucas <lucas456@hotmail.com>

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import VeeValidate from 'vee-validate';
 import ToggleButton from 'vue-js-toggle-button';
 import App from './App.vue';
 import router from './router';
+import VueChart from 'vue-chart-js';
+// import { Bar,Line } from 'vue-chart-js';
 
 Vue.use(ToggleButton);
 Vue.use(VeeValidate);
+Vue.use(VueChart);
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
@@ -15,5 +19,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App },
+  components: { App, VueChart },
 });
