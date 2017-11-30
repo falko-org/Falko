d19af032f31590cbec491f2732b5b295a033f31b
Matheus Roberto <matheus456456@gmail.com>
2017-11-30 04:20:02 -0200

    Fixing codeclimate issue
    Signed-off-by: Matheus Roberto <matheus456456@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueChart from 'vue-chart-js';
 import VeeValidate from 'vee-validate';
 import ToggleButton from 'vue-js-toggle-button';
 import App from './App.vue';
 import router from './router';
-import VueChart from 'vue-chart-js';
 // import { Bar,Line } from 'vue-chart-js';
 
 Vue.use(ToggleButton);
