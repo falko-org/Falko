875f57dcacce00ba783991942d6def54ed190d2c
Daniel Oda <danieloda35@gmail.com>
2017-12-12 16:02:02 -0200

    Fixing dashboard apearance in non github projects

--- src/components/Projects/Project.vue
+++ src/components/Projects/Project.vue
@@ -6,28 +6,28 @@
           <div class="card-body text-center">
             <div class="row">
               <div class="col">
-                <h1 class="card-title"><i class="fa fa-github" aria-hidden="true" v-if="isFromProjectGitHub()"></i>&nbsp;{{project.name}}</h1>
-                <p class="card-text text-muted">{{project.description}}</p>
+                <h1 class="card-title" align="left"><i class="fa fa-github" aria-hidden="true" v-if="isFromProjectGitHub()"></i>&nbsp;{{project.name}}</h1>
+                <h4 class="card-text text-muted" align="left">&nbsp;&nbsp;{{project.description}}</h4>
               </div>
                 <div class="row">
-                  <div v-bind:class="divClass()" align="center">
+                  <div align="center">
                     <router-link v-bind:to="'/projects/'+project.id+'/issues'">
                       <button type="button" class="btn btn-info btn-md falko-button" v-if="isFromProjectGitHub()">
                         Backlog
                       </button>
                     </router-link>
                   </div>
-                  <div v-bind:class="divClass()" align="center">
+                  <div align="center">
                     <router-link v-bind:to="'/projects/'+project.id+'/releases'">
                       <button type="button" class="btn btn-info btn-md falko-button">
                         Releases
                       </button>
                     </router-link>
                   </div>
-                  <div v-bind:class="divClass()" align="center">
+                  <div align="center">
                     <edit-project v-on:edited-project="refreshProject()"></edit-project>
                   </div>
-                  <div v-bind:class="divClass()" align="center">
+                  <div align="center">
                     <div class="row">
                       <delete-project></delete-project>
                     </div>
@@ -38,7 +38,7 @@
           </div>
         </div>
       </div>
-      <div class="row justify-content-center">
+      <div class="row justify-content-center" v-if="isFromProjectGitHub()">
         <div class="col-9" align="center">
           <issues-graphic></issues-graphic>
         </div>
@@ -96,12 +96,6 @@ export default {
       localStorage.setItem('is_project_from_github', 'false');
       return false;
     },
-    divClass() {
-      if (this.isFromProjectGitHub()) {
-        return 'col-md-3';
-      }
-      return 'col-md-2';
-    },
   },
   mounted() {
     this.getProject(this.$route.params.id);
@@ -117,6 +111,7 @@ export default {
 }
 .btn {
   width: 120px;
+  margin-right: 4px;
 }
 .card {
   align-self: center;
@@ -125,9 +120,6 @@ export default {
 }
 h1 {
   color: #598392;
-}
-.card-title {
-  text-align: left;
 }
 
 #dashBoard {
