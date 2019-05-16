3953ae5c1511ed67b0cbb490fac3c8ce6499d72c
danielmarques28 <danielmarques7@hotmail.com>
2019-05-16 15:29:49 -0300

    Installing vuetify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import VueChart from 'vue-chart-js';
 import VeeValidate from 'vee-validate';
 import ToggleButton from 'vue-js-toggle-button';
 import App from './App.vue';
 import router from './router';
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 
+Vue.use(Vuetify);
 Vue.use(ToggleButton);
 Vue.use(VeeValidate);
 Vue.use(VueChart);
